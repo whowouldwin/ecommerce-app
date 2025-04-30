9783e5aff132ac041af532e17f309c47d30f99a6
whowouldwin <leranetwork@gmail.com>
2025-04-30 23:40:13 +0300

    feat: RSS-ECOMM-1 Repository Setup

---
+++ eslint.config.js
@@ -0,0 +1,51 @@
+import js from '@eslint/js'
+import globals from 'globals'
+import react from 'eslint-plugin-react'
+import reactHooks from 'eslint-plugin-react-hooks'
+import importPlugin from 'eslint-plugin-import'
+import reactRefresh from 'eslint-plugin-react-refresh'
+import tseslint from 'typescript-eslint'
+import prettier from 'eslint-config-prettier';
+
+export default tseslint.config(
+  { ignores: ['dist'] },
+  {
+    extends: [js.configs.recommended, ...tseslint.configs.recommended],
+    files: ['**/*.{ts,tsx}'],
+    languageOptions: {
+      ecmaVersion: 2020,
+      sourceType: 'module',
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+      }
+    },
+    plugins: {
+      react,
+      'react-hooks': reactHooks,
+      'react-refresh': reactRefresh,
+      import: importPlugin,
+    },
+    settings: {
+      react: {
+        version: 'detect',
+      },
+    },
+    rules: {
+      ...js.configs.recommended.rules,
+      ...react.configs.recommended.rules,
+      ...reactHooks.configs.recommended.rules,
+      'react-refresh/only-export-components': [
+        'warn',
+        { allowConstantExport: true },
+      ],
+      'react/jsx-no-target-blank': ['warn', { enforceDynamicLinks: 'always' }],
+      'import/order': ['warn', { groups: ['builtin', 'external', 'internal']}],
+      'react/react-in-jsx-scope': 'off',
+    },
+  },
+  {
+    name: 'prettier-config',
+    rules: prettier.rules,
+  }
+)
