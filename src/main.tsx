1525fa0c8627d4006df5523a8932953f7ff99351
Нина Евлаш <evlash.nina@mail.ru>
2025-05-14 12:01:23 +0200

    feat: add chakra UI in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,18 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import './index.css';
-import App from './App.tsx';
+import * as React from 'react'
+import { ChakraProvider, ColorModeScript } from '@chakra-ui/react'
+import * as ReactDOM from 'react-dom/client'
+import App from './App'
+import theme from './theme'
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-);
+const rootElement = document.getElementById('root')
+if (rootElement) {
+  ReactDOM.createRoot(rootElement).render(
+    <React.StrictMode>
+      <ChakraProvider theme={theme}>
+        <ColorModeScript initialColorMode={theme.config.initialColorMode} />
+        <App />
+      </ChakraProvider>
+    </React.StrictMode>,
+  )
+}
+
