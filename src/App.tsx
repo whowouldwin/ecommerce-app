ec0d0d33c9b8993c9b441e2655061294ed414f3d
Нина Евлаш <evlash.nina@mail.ru>
2025-05-15 14:09:12 +0200

    refactor: decompose Layout.tsx into Header, NavLinks, BurgerMenu, Logo, AuthButtons

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Layout from './components/Layout';
+import Layout from './components/Layout/Layout';
 import MainPage from './pages/MainPage';
 import LoginPage from './pages/LoginPage';
 import RegisterPage from './pages/RegisterPage';
