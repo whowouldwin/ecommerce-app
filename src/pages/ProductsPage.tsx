cc4fa1f273506c783c35a5c781d3a6c0aea771fe
whowouldwin <leranetwork@gmail.com>
2025-05-31 20:17:43 +0300

    fix: add filter by name

--- src/pages/ProductsPage.tsx
+++ src/pages/ProductsPage.tsx
@@ -150,8 +150,8 @@ const ProductsPage = () => {
                 width="200px"
               >
                 <option value="">Default</option>
-                <option value="name asc">Name: A to Z</option>
-                <option value="name desc">Name: Z to A</option>
+                <option value="name.en-US asc">Name: A to Z</option>
+                <option value="name.en-US desc">Name: Z to A</option>
                 <option value="price asc">Price: Low to High</option>
                 <option value="price desc">Price: High to Low</option>
               </Select>
