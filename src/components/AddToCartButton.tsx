4c862d1c254efebfc1a8948a612e0e8cbe3f434c
privatePython <gitarist.pasha@gmail.com>
2025-06-10 16:32:55 +0300

    feat: add debounce function

--- src/components/AddToCartButton.tsx
+++ src/components/AddToCartButton.tsx
@@ -1,6 +1,7 @@
 import { Button, HStack, Text, IconButton } from '@chakra-ui/react';
 import { MinusIcon, AddIcon } from '@chakra-ui/icons';
 import { useDispatch, useSelector } from 'react-redux';
+import { useEffect, useState } from 'react';
 import type { AppDispatch } from '../store/store';
 import {
   addLineItem,
@@ -18,20 +19,40 @@ const AddToCartButton = ({ productId, variantId }: Props) => {
   const lineItem = useSelector(selectCartLineItems).find(
     (li) => li.productId === productId && li.variant.id === variantId,
   );
-  const qty = lineItem?.quantity ?? 0;
+  const [debounceTimer, setDebounceTimer] = useState(0);
+
+  const [productQty, setProductQty] = useState(lineItem?.quantity ?? 0);
+  useEffect(() => {
+    setProductQty(lineItem?.quantity ?? 0);
+  }, [lineItem?.quantity]);
+
+  const debouncedCounterFunc: (qty: number) => void = (qty) => {
+    if (debounceTimer) {
+      clearTimeout(debounceTimer);
+    }
+    const timerID = setTimeout(() => {
+      dispatch(
+        changeLineItemQuantity({ lineItemId: lineItem!.id, quantity: qty }),
+      );
+    }, 300);
+    setDebounceTimer(timerID);
+  };
 
   const handleAdd = () =>
     dispatch(addLineItem({ productId, variantId, quantity: 1 }));
-  const handlePlus = () =>
-    dispatch(
-      changeLineItemQuantity({ lineItemId: lineItem!.id, quantity: qty + 1 }),
-    );
-  const handleMinus = () =>
-    dispatch(
-      changeLineItemQuantity({ lineItemId: lineItem!.id, quantity: qty - 1 }),
-    );
+  const handlePlus = () => {
+    const value = productQty + 1;
+    setProductQty(value);
+    debouncedCounterFunc(value);
+  };
 
-  if (qty === 0) {
+  const handleMinus = () => {
+    const value = productQty - 1;
+    setProductQty(value);
+    debouncedCounterFunc(value);
+  };
+
+  if (productQty === 0) {
     return (
       <Button colorScheme="green" size="sm" onClick={handleAdd}>
         Add&nbsp;to&nbsp;Cart
@@ -46,9 +67,9 @@ const AddToCartButton = ({ productId, variantId }: Props) => {
         icon={<MinusIcon />}
         size="xs"
         onClick={handleMinus}
-        isDisabled={qty === 0}
+        isDisabled={productQty === 0}
       />
-      <Text fontWeight="semibold">{qty}</Text>
+      <Text fontWeight="semibold">{productQty}</Text>
       <IconButton
         aria-label="Increase"
         icon={<AddIcon />}
