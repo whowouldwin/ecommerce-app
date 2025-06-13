73f2431b9c574a216b4d16c3b7c25fb016804398
privatePython <gitarist.pasha@gmail.com>
2025-06-13 16:35:23 +0300

    feat: add discount slice and discount list component

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -3,6 +3,7 @@ import { Box } from '@chakra-ui/react';
 import HeroSection from '../components/MainPage/HeroSection.tsx';
 import CTASection from '../components/MainPage/CTASection.tsx';
 import ProductCategories from '../components/MainPage/ProductCategories.tsx';
+import DiscountList from './DiscountList.tsx';
 
 const MainPage = () => {
   return (
@@ -11,6 +12,7 @@ const MainPage = () => {
       <HeroSection />
       <CTASection />
       <ProductCategories />
+      <DiscountList />
     </Box>
   );
 };
