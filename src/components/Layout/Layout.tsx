66fdc26fa4ce0763737b2a6610a61973abfb9bee
privatePython <gitarist.pasha@gmail.com>
2025-06-25 16:33:13 +0300

    feat: add Loader

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,6 +1,9 @@
 import { Box, Container } from '@chakra-ui/react';
 import { ReactNode } from 'react';
+import { useSelector } from 'react-redux';
 import Header from './Header';
+import { selectIsCartUpdating } from '../../features/cart/cartSlice.ts';
+import Loader from '../Loader.tsx';
 
 interface LayoutProps {
   children: ReactNode;
@@ -8,12 +11,14 @@ interface LayoutProps {
 }
 
 const Layout = ({ children }: LayoutProps) => {
+  const isUpdating = useSelector(selectIsCartUpdating);
   return (
     <Box minH="100vh">
       <Header />
       <Container as="main" maxW="container.xl" py={6}>
         {children}
       </Container>
+      {isUpdating && <Loader />}
     </Box>
   );
 };
